82d03e88c583f3ee45cdefc0c956accdb139f60a
Denver987 <142704224+Nanovichi@users.noreply.github.com>
2024-02-07 23:52:37 +0100

    update sghira

--- Assets/scripts/Interactions/Paper.cs
+++ Assets/scripts/Interactions/Paper.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Paper : MonoBehaviour, IInteractable
 {
     [SerializeField]
     private GameObject paper;
+
+    [SerializeField]
+    private TextMeshProUGUI noteText;
+
+    [SerializeField,TextArea(3, 10)]
+    private string realText;
 
     private ControlPlayer player;
 
@@ -15,8 +22,10 @@ public class Paper : MonoBehaviour, IInteractable
     }
     public void Interact()
     {
+
         AudioManager.Instance.PlayOneShot(FmodEvents.Instance.papperPickup,this.transform.position);
         paper.SetActive(true);
+        noteText.text = realText;
         player.enabled = false;
 
     }
