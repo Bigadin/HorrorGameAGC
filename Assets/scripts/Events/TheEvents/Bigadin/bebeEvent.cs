dea75280a9cc021d3e8e22952b2463e87ca95b11
Bigadin <ggyannn@gmail.com>
2024-02-09 19:42:05 +0100

    bebe event

--- Assets/scripts/Events/TheEvents/Bigadin/bebeEvent.cs
+++ Assets/scripts/Events/TheEvents/Bigadin/bebeEvent.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bebeEvent : MonoBehaviour
+public class bebeEvent : GameEvent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] Door roomDoor;
+    [SerializeField] Animator[] LightAnim;
+
+    bool isEventStart = false;
+    public override void ConcreteEvent()
+    {
+        base.ConcreteEvent();
+        if (!isEventStart)
+        {
+            StartCoroutine(WaitbeforeStart());
+        }
+
+    }
+    IEnumerator WaitbeforeStart()
     {
         
+        yield return new WaitForSeconds(10);
+        isEventStart = true;
+        // babe sound
+        
+        foreach (Animator anim in LightAnim)
+        {
+            anim.Play("DoorEvent");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (isEventStart)
+            {
+                roomDoor.CloseDoor();
+                roomDoor.setdoorStat(Door.DoorState.Locked);
+                StartCoroutine(WaitbeforeEnd());
+                isEventStart=false;
+            }
+
+        }
+    }
+    IEnumerator WaitbeforeEnd()
+    {
+        yield return new WaitForSeconds(10);
+        roomDoor.setdoorStat(Door.DoorState.Unlocked);
+        LightAnim[0].enabled = false;
+        LightAnim[1].enabled = false;
     }
 }
